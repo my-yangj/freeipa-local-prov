6aaf42faf932a94435fc75d3b3b42c8ed80dfd87
theta.yang <32123458+my-yangj@users.noreply.github.com>
2021-02-21 21:34:27 +0800

    Update runit.sh
    devtoolset-4 is obsoleted and remove it.

--- runit.sh
+++ runit.sh
@@ -8,7 +8,6 @@ docker-compose run --rm -u ubu centos bash
 # sudo yum install llvm-toolset-7 -y
 # sudo yum install rh-git218 -y
 # sudo yum install devtoolset-7 -y
-# sudo yum install devtoolset-4 -y
 # sudo yum install rh-perl526 -y
 # sudo yum install rh-python36 -y
 # sudo yum install devtoolset-8 -y
