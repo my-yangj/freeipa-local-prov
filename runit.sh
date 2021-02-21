f722a8b5b01ad9fcd8200880ab5724ddf429cc95
theta.yang <32123458+my-yangj@users.noreply.github.com>
2021-02-21 21:22:11 +0800

    Update runit.sh

--- runit.sh
+++ runit.sh
@@ -1,2 +1,16 @@
 docker-compose up
 docker-compose run --rm -u ubu centos bash
+
+# install rh toolset to mounted /opt
+# https://www.softwarecollections.org/en/scls/rhscl/go-toolset-7/
+# sudo yum install centos-release-scl
+# sudo yum install rh-ruby26 -y
+# sudo yum install llvm-toolset-7 -y
+# sudo yum install rh-git218 -y
+# sudo yum install devtoolset-7 -y
+# sudo yum install devtoolset-4 -y
+# sudo yum install rh-perl526 -y
+# sudo yum install rh-python36 -y
+# sudo yum install devtoolset-8 -y
+# sudo yum install rust-toolset-7 -y
+# sudo yum install go-toolset-7 -y
